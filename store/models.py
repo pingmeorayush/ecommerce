de43376b1f74b816284361c36f79b037410272c1
Ayush Chauhan <pingmeorayush@gmail.com>
2022-02-10 17:14:12 +0530

    customer wish list completed

--- store/models.py
+++ store/models.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 from mptt.models import MPTTModel, TreeForeignKey
 
+from django.conf import settings
 
 class Category(MPTTModel):
     """
@@ -110,6 +111,7 @@ class Product(models.Model):
     )
     created_at = models.DateTimeField(_("Created at"), auto_now_add=True, editable=False)
     updated_at = models.DateTimeField(_("Updated at"), auto_now=True)
+    users_wishlist = models.ManyToManyField(settings.AUTH_USER_MODEL,related_name='user_wishlist',blank=True)
 
     class Meta:
         ordering = ("-created_at",)
